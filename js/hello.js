8e1dfac05dc2008080e84b35a62832e0fcfe4a73
aahartley <aahartley2000@gmail.com>
2020-10-07 13:12:01 -0400

    1 file modified

--- js/hello.js
+++ js/hello.js
@@ -1,1 +1,19 @@
+
+/* Send a customized message based on time
+*/
 console.log("im in hello.js");
+
+//get the date and time
+var today= new Date();
+var hoursNow = today.getHours();
+
+//display the greeting message
+var greeting = "";
+if(hoursNow <= 12){
+  greeting= "Good Morning";
+}else{
+  greeting="Good Afternoon";
+}
+
+//write to the document
+document.write(greeting);
