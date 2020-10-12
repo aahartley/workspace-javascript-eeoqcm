779f2615461ab57a8ac5de1d54140ee937d9423f
aahartley <aahartley2000@gmail.com>
2020-10-12 13:03:35 -0400

    1 file modified, 1 added

---
+++ js/arith.js
@@ -0,0 +1,7 @@
+//get the element
+//elemnt -- property 
+var num1= parseInt(document.getElementById('n1').textContent);
+var num2= parseInt(document.getElementById('n2').textContent);
+var c = num1 + num2;
+document.write(c);
+
